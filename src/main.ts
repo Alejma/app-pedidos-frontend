2498e8723bd28e7f241f70f9a73a359b92927fcf
Alejandro Marin <alejma7@gmail.com>
2022-10-15 18:20:24 -0500

    Version 1.0.0 Frontend App Pedidos

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
